0b9cd1a0d1df8a9891311bd36feb116f500d7a1c
Felipe Cocco <felipecocco@gmail.com>
2019-12-09 16:59:49 -0500

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,26 +1,209 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { findByLabelText } from "@testing-library/dom";
+let stuff = new Array();
 
-function App() {
+const App = props => {
+  let student = props.student;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div class="studentContainer">
+      <div class="studentDetails">
+        <div>
+          Student: <b>{student.name}</b>
+        </div>
+        <div>Teacher: {student.teacher}</div>
+      </div>
+      <div class="studentDetails">
+        <div>
+          Days Absent: {student.absent}
+          <br></br>
+          Days Tardy: {student.tardy}
+        </div>
+        <div>Report Period: {student.reportPeriod}</div>
+      </div>
+      <div class="explanation">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The purpose of this report card is to provide you with information
+          about your child’s learning and growth in school. Our aim is for this
+          report to complement the parent-teacher conferences and other
+          communications you have with your teachers. To best assist academic
+          progress, we ask that you share this information with your child in a
+          constructive and supportive manner. We recommend that you pick out a
+          few strengths and challenges for you and your child to think about.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          The skills and concepts listed in this report represent what we expect
+          students to acquire by the end of the year. We have chosen three
+          indicators to represent each student’s current performance within
+          these skills and concepts. For clarity, the performance indicators
+          should be understood as the following.
+        </p>
+        <p>
+          <b>Not Yet Developed</b> - Student rarely displays use of skills or
+          concepts without teacher support. This is an area of concern. This
+          means that specific attention is being placed on that skill or concept
+          for your child.
+        </p>
+        <p>
+          <b>Developing</b> - Student is progressing in their independent use of
+          skills and concepts while still require- ing some teacher support.
+        </p>
+        <p>
+          <b>Independent</b> - Student displays consistent and independent use
+          of skills or concepts.
+        </p>
+        <p>
+          <b>N/A</b> - Skills of concepts not applicable to this marking period.
+        </p>
+      </div>
+      <h2
+        style={{ display: "none" }}
+      >{`${student.name.toUpperCase()} - ${student.grade.toUpperCase()}`}</h2>
+      {student.subjects.map(subj => (
+        <Fragment>
+          <div class="subjectTitle">{subj.title}</div>
+          {subj.blurb.length > 0 && <div class="blurb">{subj.blurb}</div>}
+          <div class="descriptors">
+            <div>
+              <i>Performance Indicators:</i>
+            </div>
+            <div>
+              <b>N</b> = Not Yet Developed
+            </div>
+            <div>
+              <b>D</b> = Developing
+            </div>
+            <div>
+              <b>I</b> = Independent
+            </div>
+            <div>
+              <b>N/A</b> = Not Applicable
+            </div>
+          </div>
+          <table class="checklistOverall">
+            <col style={{ width: "55%" }} />
+            <colgroup span="4"></colgroup>
+            <colgroup span="4"></colgroup>
+            <thead>
+              <tr>
+                <td rowspan="2" style={{ width: "55%" }}></td>
+                <th
+                  colspan="4"
+                  scope="colgroup"
+                  style={{
+                    marginTop: "20px",
+                    width: "20%",
+                    color: "white",
+                    backgroundColor: "#f54530",
+                    padding: "5px",
+                    fontWeight: "400",
+                    borderLeft: "4px solid black",
+                    borderRight: "4px solid black",
+                    borderTop: "2px solid black"
+                  }}
+                >
+                  WINTER
+                </th>
+                <th
+                  colspan="4"
+                  scope="colgroup"
+                  style={{
+                    marginTop: "20px",
+                    width: "20%",
+                    color: "white",
+                    fontWeight: "400",
+                    backgroundColor: "#f54530",
+                    padding: "5px",
+                    borderLeft: "4px solid black",
+                    borderRight: "4px solid black",
+                    borderTop: "2px solid black"
+                  }}
+                >
+                  SPRING
+                </th>
+              </tr>
+              <tr>
+                <th class="individualCell leftEdge">N</th>
+                <th class="individualCell">D</th>
+                <th class="individualCell">I</th>
+                <th class="individualCell rightEdge">N/A</th>
+                <th class="individualCell leftEdge">N</th>
+                <th class="individualCell">D</th>
+                <th class="individualCell">I</th>
+                <th class="individualCell rightEdge">N/A</th>
+              </tr>
+            </thead>
+            {subj.checklist.map(category => (
+              <Fragment>
+                <tr class="checklistCategory ">
+                  <th class="checklistDescriptors heading" scope="row">
+                    {category.name}
+                  </th>
+                  <td style={{ borderLeft: "4px solid black" }}></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td style={{ borderLeft: "4px solid black" }}></td>
+                  <td></td>
+                  <td></td>
+                  <td style={{ borderRight: "4px solid black" }}></td>
+                </tr>
+                {category.items.map(item => (
+                  <tr class="checklistDescriptors">
+                    <th
+                      scope="row"
+                      style={{
+                        textAlign: "left",
+                        fontWeight: "100",
+                        padding: "5px"
+                      }}
+                    >
+                      {item.description}
+                    </th>
+                    {["N", "D", "I", "N/A"].map((score, i) => (
+                      <td
+                        className={`individualCell ${
+                          i == 0 ? "leftEdge" : " "
+                        } ${i == 3 ? "rightEdge" : ""}`}
+                      >
+                        {score == item.winter_score ? "X" : ""}
+                      </td>
+                    ))}
+                    {["N", "D", "I", "N/A"].map((score, i) => (
+                      <td
+                        className={`individualCell ${
+                          i == 0 ? "leftEdge" : " "
+                        } ${i == 3 ? "rightEdge" : ""}`}
+                      >
+                        {score == item.spring_score ? "X" : ""}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </Fragment>
+            ))}
+          </table>
+          <div>
+            {subj.winterComments.length > 0 && (
+              <div class="comment">
+                <div class="header">WINTER COMMENTS:</div>
+                <div>{subj.winterComments}</div>
+              </div>
+            )}
+            {subj.springComments.length > 0 && (
+              <div class="comment">
+                <div class="header">SPRING COMMENTS:</div>
+                <div>{subj.springComments}</div>
+              </div>
+            )}
+          </div>
+        </Fragment>
+      ))}
+
+      <div class="end"></div>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,174 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let Student1 = {
+  name: "Camilla Sarraf-Mueller",
+  grade: "Grade 1",
+  absent: 0,
+  tardy: 5,
+  teacher: "Melinda Moore",
+  reportPeriod: "September 5, 2019 - January 15, 2020",
+
+  subjects: [
+    {
+      title: "Behavior and Work Habits",
+      blurb: "",
+      winterComments:
+        "Tania needs a bit of work. She plays well with others when she wants but is very particular about who she plays with and whether or not she will participate in group activities. Tania is a fast talker. She can out-talk most of the children in the class and usually does. This makes her popular and unpopular at the same time. We are working on Tania’s listening and sharing skills. She is a quick learner and often is way ahead of the other students in grasping difficult content. We enjoy Tania quite a bit.",
+      springComments: "",
+      checklist: [
+        {
+          name: "Behavior",
+          items: [
+            {
+              description: "Relates well with peers",
+              winter_score: "I",
+              spring_score: "N/A"
+            },
+            {
+              description: "Relates well with adults",
+              winter_score: "D",
+              spring_score: ""
+            },
+            {
+              description: "Participates in group activities",
+              winter_score: "D",
+              spring_score: ""
+            },
+            {
+              description: "Demonstrates self-control",
+              winter_score: "D",
+              spring_score: ""
+            },
+            {
+              description: "Observes class routines",
+              winter_score: "D",
+              spring_score: ""
+            }
+          ]
+        },
+        {
+          name: "Work Habits",
+          items: [
+            {
+              description: "Works independently and productively",
+              winter_score: "I",
+              spring_score: "N/A"
+            },
+            {
+              description: "Cooperates in small group/partnership work",
+              winter_score: "D",
+              spring_score: ""
+            },
+            {
+              description: "Cooperates in large group work",
+              winter_score: "I",
+              spring_score: "N/A"
+            },
+            {
+              description: "Is attentive during class instruction",
+              winter_score: "D",
+              spring_score: ""
+            },
+            {
+              description:
+                "Participates during class discussion and activities",
+              winter_score: "I",
+              spring_score: "N/A"
+            },
+            {
+              description:
+                "Is able to repond to the work and the ideas of others",
+              winter_score: "D",
+              spring_score: ""
+            },
+            {
+              description: "Follows directions",
+              winter_score: "I",
+              spring_score: "N/A"
+            },
+            {
+              description: "Sustains effort",
+              winter_score: "D",
+              spring_score: ""
+            },
+            {
+              description: "Organizes and uses materials well",
+              winter_score: "I",
+              spring_score: "N/A"
+            },
+            {
+              description: "Completes class assignments",
+              winter_score: "D",
+              spring_score: ""
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "Language Arts - Writing",
+      blurb:
+        "Writing work in first grade focuses on children taking all the crative and wonderful ideas that are in their heads and getting them onto paper. Through planning and illustrating, students spend time developing their ideas before beginning their initial drafts. Numerous opportunities are given for both self-directed and structured writing pieces that take the students through each step of the writing process. As students grow as spellers they are encouraged to take risks and use invented spelling, while also applying the sight words and spelling patterns covered in class. Students polish their work a they continue to practice mechanics, handwriting and overall presentation.",
+      winterComments:
+        "Tania needs a bit of work. She plays well with others when she wants but is very particular about who she plays with and whether or not she will participate in group activities. Tania is a fast talker. She can out-talk most of the children in the class and usually does. This makes her popular and unpopular at the same time. We are working on Tania’s listening and sharing skills. She is a quick learner and often is way ahead of the other students in grasping difficult content. We enjoy Tania quite a bit.",
+      springComments: "",
+      checklist: [
+        {
+          name: "Writing",
+          items: [
+            {
+              description:
+                "Is able to hold a complete thought and put it into written form",
+              spring_score: "I",
+              winter_score: ""
+            },
+            {
+              description: "Can read their own writing",
+              spring_score: "I",
+              winter_score: ""
+            },
+            {
+              description: "Uses writing structure (Beginning, Middle, End)",
+              spring_score: "I",
+              winter_score: ""
+            },
+            {
+              description: "Is wiling to take risks with phonetic spelling",
+              spring_score: "I",
+              winter_score: ""
+            },
+            {
+              description: "Correctly spells practiced sight words",
+              spring_score: "I",
+              winter_score: ""
+            },
+            {
+              description: "Leaves spaced between words",
+              spring_score: "I",
+              winter_score: ""
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+let Student2 = {
+  ...Student1,
+  name: "Albert Swanson",
+  grade: "GRADE 4",
+  tardy: 20,
+  teacher: "Maggie Morrison"
+};
+
+ReactDOM.render(
+  [<App student={Student1} />, <App student={Student2} />],
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
